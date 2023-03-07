53647a4aee403a6c16f7f45e7b65e768af904cbb
Lunamorgendorffer <nabdou68@gmail.com>
2023-03-07 16:16:37 +0100

    script pour le message

--- 3.Morpion/script.js
+++ 3.Morpion/script.js
@@ -1,13 +1,16 @@
-var cases = document.querySelectorAll('.case');
+var cases = document.querySelectorAll('.case'); //sélectionne tous les éléments avec la classe "case" 
 
 var tour = "x";
 
+var message = document.getElementById('message');// recupere l'element par id message 
+
 cases.forEach(function(case_) {
     case_.addEventListener('click', function() {
-        if (case_.textContent === "") {
-            case_.textContent = tour;
+        if (case_.textContent === "") { //Lorsqu'une case est cliquée, on vérifie si elle est vide 
+            case_.textContent = tour;//Si c'est vide, il faut ajouter le texte 
             case_.classList.add(tour);
             tour = (tour === "x") ? "o" : "x";
+            message.textContent = "C'est au tour de " + tour.toUpperCase();
         }
     });
 });
